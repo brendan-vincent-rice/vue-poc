ca75e123ae8d4f63bfad7c84f548cf88c0b6bbdd
Brendan Rice <brendan.rice@insight.com>
2020-05-05 07:15:42 -0400

    Add "build a bot" Vue.js files

---
+++ build-a-bot/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/airbnb',
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'line-break-style': 0, // Windows line endings
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+};
